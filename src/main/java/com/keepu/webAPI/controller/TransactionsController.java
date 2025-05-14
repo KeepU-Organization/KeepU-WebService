972eb69b7ee637b53917bfe7ba54668bb4b72af7
Diego Antonio Salinas Casaico <U202210161@upc.edu.pe>
2025-05-14 18:11:25 -0500

    Funcionalidad de la lista de transacciones y filtros
    Hizo la funcionalidad del historial y sus filtros. (falta de probar aun)

--- src/main/java/com/keepu/webAPI/controller/TransactionsController.java
+++ src/main/java/com/keepu/webAPI/controller/TransactionsController.java
@@ -2,11 +2,15 @@ package com.keepu.webAPI.controller;
 
 import com.keepu.webAPI.dto.request.CreateTransactionRequest;
 import com.keepu.webAPI.dto.response.TransactionResponse;
+import com.keepu.webAPI.model.enums.TransactionType;
 import com.keepu.webAPI.service.TransactionsService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/transactions")
@@ -24,4 +28,22 @@ public class TransactionsController {
     public ResponseEntity<TransactionResponse> getById(@PathVariable Integer id) {
         return ResponseEntity.ok(transactionsService.getTransactionById(id));
     }
+
+    @GetMapping("/wallet/{walletId}")
+    public List<TransactionResponse> getTransactionsByWalletId(@PathVariable String walletId) {
+        return transactionsService.getTransactionsByWalletId(walletId);
+    }
+
+    @GetMapping("/wallet/{walletId}/filter")
+    public List<TransactionResponse> getFilteredTransactions(
+            @PathVariable String walletId,
+            @RequestParam(required = false) TransactionType type,
+            @RequestParam(required = false) LocalDateTime startDate,
+            @RequestParam(required = false) LocalDateTime endDate,
+            @RequestParam(required = false) Double minAmount,
+            @RequestParam(required = false) Double maxAmount
+    ) {
+        return transactionsService.getFilteredTransactionsByWalletId(walletId, type, startDate, endDate, minAmount, maxAmount);
+    }
+
 }
--- src/main/java/com/keepu/webAPI/service/TransactionsService.java
+++ src/main/java/com/keepu/webAPI/service/TransactionsService.java
@@ -6,11 +6,15 @@ import com.keepu.webAPI.exception.NotFoundException;
 import com.keepu.webAPI.mapper.TransactionsMapper;
 import com.keepu.webAPI.model.Transactions;
 import com.keepu.webAPI.model.Wallet;
+import com.keepu.webAPI.model.enums.TransactionType;
 import com.keepu.webAPI.repository.TransactionsRepository;
 import com.keepu.webAPI.repository.WalletRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -36,5 +40,37 @@ public class TransactionsService {
         Transactions transaction = transactionsRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Transacción no encontrada"));
         return transactionsMapper.toTransactionResponse(transaction);
+    }
+
+    @Transactional(readOnly = true)
+    public List<TransactionResponse> getTransactionsByWalletId(String walletId) {
+        Wallet wallet = walletRepository.findByWalletId(walletId)
+                .orElseThrow(() -> new NotFoundException("Billetera no encontrada"));
+
+        List<Transactions> transactions = transactionsRepository.findByWallet(wallet);
+
+        return transactions.stream()
+                .map(transactionsMapper::toTransactionResponse)
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<TransactionResponse> getFilteredTransactionsByWalletId( String walletId, TransactionType type,
+            LocalDateTime startDate,
+            LocalDateTime endDate,
+            Double minAmount,
+            Double maxAmount
+    )
+    {
+        Wallet wallet = walletRepository.findByWalletId(walletId)
+                .orElseThrow(() -> new NotFoundException("Billetera no encontrada"));
+
+        List<Transactions> transactions = transactionsRepository.findFilteredTransactions(
+                wallet, type, startDate, endDate, minAmount, maxAmount
+        );
+
+        return transactions.stream()
+                .map(transactionsMapper::toTransactionResponse)
+                .toList();
     }
 }
