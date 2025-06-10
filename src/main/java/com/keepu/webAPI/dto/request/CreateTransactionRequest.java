2bbe9ad4bdddacb1bf1127dd2b2c7ce0074f58b1
Salvador Diaz Aguirre <u202216148@upc.edu.pe>
2025-06-09 23:10:21 -0500

    H3: transferencia

--- src/main/java/com/keepu/webAPI/dto/request/CreateTransactionRequest.java
+++ src/main/java/com/keepu/webAPI/dto/request/CreateTransactionRequest.java
@@ -6,6 +6,8 @@ import com.keepu.webAPI.model.Wallet;
 import com.keepu.webAPI.model.enums.TransactionType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record CreateTransactionRequest(
@@ -14,7 +16,7 @@ public record CreateTransactionRequest(
         Wallet wallet,
 
         @NotNull(message = "El monto no puede ser nulo")
-        Double amount,
+        BigDecimal amount,
 
         @NotBlank(message = "La descripción no puede estar vacía")
         String description,
